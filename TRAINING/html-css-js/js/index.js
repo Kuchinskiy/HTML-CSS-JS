5865e9f16715099bf5bc35f4bb73a9acc9342283
Sergey <domarch388640@gmail.com>
2019-07-19 17:30:37 +0300

    commit

--- TRAINING/html-css-js/js/index.js
+++ TRAINING/html-css-js/js/index.js
@@ -1,8 +1,54 @@
 // DEFAULTS
 'use strict';
 
-var name = 'Nick';
-console.log('Hello ' + name);
-if (name.length > 7) {
-    console.log('Wow, you have a REALLY long name!');
-}
+
+// PRACTICE
+
+// var name = 'Nick';
+// console.log('Hello ' + name);
+// if (name.length > 7) {
+//     console.log('Wow, you have a REALLY long name!');
+// } else {
+//     console.log('Your name isn`t very long')
+// }
+
+
+// var lemonChicken = true;
+// var beefWithBlackBean = false;
+// var sweetAndSourPork = true;
+
+// if (lemonChicken) {
+//     console.log('Great! I`m having lemon chicken!');
+// } else if (beefWithBlackBean) {
+//     console.log('I`m having the beef.');
+// } else if (sweetAndSourPork) {
+//     console.log('OK, I`ll have the pork.');
+// } else {
+//     console.log('Well, I guess I`ll have rice then.');
+// }
+
+
+// PRACTICE (page 120, part A )
+
+// var name = 'Harry';
+
+// var isNotMyName = 'Nick';
+// console.log(isNotMyName === name);
+
+// var myNameIs = 'Harry';
+// console.log(myNameIs === name);
+
+// var isNotName = 'Dave';
+// console.log(isNotName === name);
+
+
+// if (isNotMyName === name) {
+//     console.log('Hi');
+// } else if (isNotName === name) {
+//     console.log('Hello');
+// } else if (isNotMyName === name) {
+//     console.log('Hello me!');
+// } else {
+//     console.log('Hello stranger');
+// }
+
