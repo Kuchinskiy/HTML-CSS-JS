78a33d36829eb6422c85f1b1581e79d6006638d9
Sergey <domarch388640@gmail.com>
2021-07-07 13:25:22 +0300

    gameOnFunction

--- TRAINING/html-css-js/js/gameGallows.js
+++ TRAINING/html-css-js/js/gameGallows.js
@@ -35,12 +35,25 @@
 */
 
 const words = ['ягуар', 'ципленок', 'макака', 'сом', 'слон'];
-const word = words[Math.floor(Math.random() * words.length)];
+// Возвращает случайно выбранное слово
+const pickWord = function () {
+	return words[Math.floor(Math.random() * words.length)];
+};
 
-let totalArr = [];
-for (let i = 0; i < word.length; i += 1) {
-	totalArr[i] = ' _ ';
-}
+const word = pickWord(); // word: загаданное слово записываем в переменную
+console.log(word);
+// answerArray: итоговый массив
+let answerArray = [];
+// Возвращает итоговый массив для заданного слова word
+const setupAnswerArray = function (word) {
+	for (let i = 0; i < word.length; i += 1) {
+		answerArray[i] = ' _ ';
+	}
+	return answerArray;
+};
+
+answerArray = setupAnswerArray(word); // итоговый массив с прилетевшим элементом с fn(pickWord)
+console.log(answerArray);
 
 /*
 * Переменная(ramainingLetters) в которую записывается(операция присвоение) 
@@ -51,39 +64,55 @@ for (let i = 0; i < word.length; i += 1) {
 * отслеживать оставшиеся к-во букв для отгадывания и уменшать(декриментировать -=1)
 * значение этой переменной на 1 если такая буква входит в слово.
 */
+
+//  showPlayerProgress: с помощью alert отображает текущее состояние игры
+const showPlayerProgress = function (answerArray) {
+	alert(answerArray.join(' '));
+};
+// showPlayerProgress(answerArray);
+
+// Запрашивает ответ игрока с помощью prompt
+const getGuess = function () {
+	prompt('Угадайте букву, или нажмите Отмена для выхода из игры :');
+};
+// getGuess(prompt);
+
+
+// remainingLetters: сколько букв осталось угадать
 let ramainingLetters = word.length;
+// Счетчик ограничивающий количество попыток для отгадывания
 let counterOfAttempts = 0;
 const limitOfAttempts = 10;
 
-while (ramainingLetters > 0 && counterOfAttempts <= limitOfAttempts) {
-	alert(totalArr.join(' '));
+// while (ramainingLetters > 0 && counterOfAttempts <= limitOfAttempts) {
+// 	alert(totalArr.join(' '));
 
-	const guess = prompt(
-		'Угадайте букву, или нажмите Отмена для выхода из игры :',
-	);
-	if (guess === null) {
-		break;
-	} else if (guess.length !== 1) {
-		alert('Введите не больше одной буквы!!!');
-	} else {
-		counterOfAttempts += 1;
-		for (let j = 0; j < word.length; j += 1) {
-			if (totalArr[j] === guess) {
-				alert('Такая буква уже была, введите другую...');
-				break;
-			}
+// 	const guess = prompt(
+// 		'Угадайте букву, или нажмите Отмена для выхода из игры :',
+// 	);
+// 	if (guess === null) {
+// 		break;
+// 	} else if (guess.length !== 1) {
+// 		alert('Введите не больше одной буквы!!!');
+// 	} else {
+// 		counterOfAttempts += 1;
+// 		for (let j = 0; j < word.length; j += 1) {
+// 			if (totalArr[j] === guess) {
+// 				alert('Такая буква уже была, введите другую...');
+// 				break;
+// 			}
 
-			if (word[j].toLowerCase() === guess.toLowerCase()) {
-				totalArr[j] = guess.toLowerCase();
-				ramainingLetters -= 1;
-			}
-		}
-	}
-}
+// 			if (word[j].toLowerCase() === guess.toLowerCase()) {
+// 				totalArr[j] = guess.toLowerCase();
+// 				ramainingLetters -= 1;
+// 			}
+// 		}
+// 	}
+// }
 
-if (ramainingLetters === 0) {
-		alert(
-		`Вы угадали все ${word.length} буквы словом было :  -->>> ${totalArr.join(
-				' ',)} <<<-- и у вас осталось еще ${counterOfAttempts} попыток)`,
-		);
-}
+// if (ramainingLetters === 0) {
+// 		alert(
+// 		`Вы угадали все ${word.length} буквы словом было :  -->>> ${totalArr.join(
+// 				' ',)} <<<-- и у вас осталось еще ${counterOfAttempts} попыток)`,
+// 		);
+// }
